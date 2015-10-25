c1e6589cfdd0d978e25c9bcae479a829bafa3d76
Maulyn Tang <tgcasce65@163.com>
2015-10-25 22:23:31 +0800

    [add] TGCFileManager to manager file business operation. [modify] Project organization.

---
+++ SyncMLLab/SyncMLLab/FileManager/TGCFileManager.swift
@@ -0,0 +1,13 @@
+//
+//  TGCFileManager.swift
+//  SyncMLLab
+//
+//  Created by DonMaulyn on 15/10/25.
+//  Copyright © 2015年 MaulynTang. All rights reserved.
+//
+
+import UIKit
+
+class TGCFileManager: NSObject {
+
+}
